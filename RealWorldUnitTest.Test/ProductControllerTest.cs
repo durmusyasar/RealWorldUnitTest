850bcf961216c7022f96ca2f7071e56954d20344
Durmuş YAŞAR <durmusyasar13@gmail.com>
2022-11-15 00:48:54 +0300

    Index, Details unit test writed

--- RealWorldUnitTest.Test/ProductControllerTest.cs
+++ RealWorldUnitTest.Test/ProductControllerTest.cs
@@ -29,5 +29,52 @@ namespace RealWorldUnitTest.Test
             var result = await _controller.Index();
             Assert.IsType<ViewResult>(result);
         }
+
+        [Fact]
+        public async void Index_ActionExecutes_ReturnProductList()
+        {
+            _mockRepo.Setup(repo => repo.GetAll()).ReturnsAsync(products);
+            var result = await _controller.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var productList = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Model);
+
+            Assert.Equal<int>(2, productList.Count());
+        }
+
+        [Fact]
+        public async void Details_IdIsNull_ReturnRedirectToIndexAction()
+        {
+            var result = await _controller.Details(null);
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+        }
+
+        [Fact]
+        public async void Details_IdInValid_ReturnNotFound()
+        {
+            Product product = null;
+            _mockRepo.Setup(repo => repo.GetById(0)).ReturnsAsync(product);
+
+            var result = await _controller.Details(0);
+            var redirect = Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, redirect.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async void Details_ValidId_ReturnProduct(int productId)
+        {
+            Product product = products.First(r => r.Id == productId);
+
+            _mockRepo.Setup(repo => repo.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.Details(productId);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var resultProduct = Assert.IsAssignableFrom<Product>(viewResult.Model);
+            Assert.Equal(product.Id, resultProduct.Id);
+            Assert.Equal(product.Name, resultProduct.Name);
+            Assert.Equal(product.Color, resultProduct.Color);
+            Assert.Equal(product.Price, resultProduct.Price);
+        }
     }
 }
